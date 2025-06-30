bf6d3dad136d8f90fd37283045600db364aa84c9
Muath Dawood <muath.dawood.89@gmail.com>
2025-06-30 13:49:16 +0300

    chor: edit service cards

--- app/[locale]/(root)/_components/Services.tsx
+++ app/[locale]/(root)/_components/Services.tsx
@@ -11,84 +11,108 @@ function Services() {
       <h1 className="text-center text-5xl font-bold text-black">
         {currentLocale === "en" ? "Services We Provide" : "الخدمات التي نقدمها"}
       </h1>
-      <div className="flex w-2/3 flex-row items-center justify-center gap-5 flex-wrap mx-auto">
-        <div className="w-1/5 bg-gray-300">
-          <div className="relative w-full aspect-square overflow-hidden">
-            <Image
-              src={images.concreteCutService}
-              alt="man cutting concrete"
-              fill
-              className="object-cover"
-            />
+      <div className="flex w-2/3 flex-row items-center justify-center gap-5 flex-wrap mx-auto mt-4">
+        <div className="w-1/4 relative aspect-square bg-gray-300">
+          <div className="absolute inset-0 flex flex-col w-full">
+            <div className="flex-[4] relative">
+              <Image
+                src={images.concreteCutService}
+                alt="man cutting concrete"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1]">
+              <p className="w-full text-center text-black p-2">
+                Lorem ipsum dolor sit.
+              </p>
+            </div>
           </div>
-          <p className="w-full text-center text-black p-2">
-            Lorem ipsum dolor sit.
-          </p>
         </div>
-        <div className="w-1/5 bg-gray-300">
-          <div className="relative w-full aspect-square overflow-hidden">
-            <Image
-              src={images.concreteCutService}
-              alt="man cutting concrete"
-              fill
-              className="object-cover"
-            />
+        <div className="w-1/4 relative aspect-square bg-gray-200">
+          <div className="absolute inset-0 flex flex-col w-full">
+            <div className="flex-[4] relative">
+              <Image
+                src={images.concreteCoreService}
+                alt="man cutting concrete"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1]">
+              <p className="w-full text-center text-black p-2">
+                Lorem ipsum dolor sit.
+              </p>
+            </div>
           </div>
-          <p className="w-full text-center text-black p-2">
-            Lorem ipsum dolor sit.
-          </p>
         </div>
-        <div className="w-1/5 bg-gray-300">
-          <div className="relative w-full aspect-square overflow-hidden">
-            <Image
-              src={images.concreteCutService}
-              alt="man cutting concrete"
-              fill
-              className="object-cover"
-            />
+        <div className="w-1/4 relative aspect-square bg-gray-200">
+          <div className="absolute inset-0 flex flex-col w-full">
+            <div className="flex-[4] relative">
+              <Image
+                src={images.demolationService}
+                alt="man cutting concrete"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1]">
+              <p className="w-full text-center text-black p-2">
+                Lorem ipsum dolor sit.
+              </p>
+            </div>
           </div>
-          <p className="w-full text-center text-black p-2">
-            Lorem ipsum dolor sit.
-          </p>
         </div>
-        <div className="w-1/5 bg-gray-300">
-          <div className="relative w-full aspect-square overflow-hidden">
-            <Image
-              src={images.concreteCutService}
-              alt="man cutting concrete"
-              fill
-              className="object-cover"
-            />
+        <div className="w-1/4 relative aspect-square bg-gray-200">
+          <div className="absolute inset-0 flex flex-col w-full">
+            <div className="flex-[4] relative">
+              <Image
+                src={images.rockCrushService}
+                alt="man cutting concrete"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1]">
+              <p className="w-full text-center text-black p-2">
+                Lorem ipsum dolor sit.
+              </p>
+            </div>
           </div>
-          <p className="w-full text-center text-black p-2">
-            Lorem ipsum dolor sit.
-          </p>
         </div>
-        <div className="w-1/5 bg-gray-300">
-          <div className="relative w-full aspect-square overflow-hidden">
-            <Image
-              src={images.concreteCutService}
-              alt="man cutting concrete"
-              fill
-              className="object-cover"
-            />
+        <div className="w-1/4 relative aspect-square bg-gray-200">
+          <div className="absolute inset-0 flex flex-col w-full">
+            <div className="flex-[4] relative">
+              <Image
+                src={images.cctvService}
+                alt="man cutting concrete"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1]">
+              <p className="w-full text-center text-black p-2">
+                Lorem ipsum dolor sit.
+              </p>
+            </div>
           </div>
-          <p className="w-full text-center text-black p-2">
-            Lorem ipsum dolor sit.
-          </p>
         </div>
-        <div className="w-1/5 bg-gray-300">
-          <div className="relative w-full aspect-square overflow-hidden">
-            <Image
-              src={images.concreteCutService}
-              alt="man cutting concrete"
-              fill
-              className="object-cover"
-            />
+        <div className="w-1/4 relative aspect-square bg-gray-200">
+          <div className="absolute inset-0 flex flex-col w-full">
+            <div className="flex-[4] relative">
+              <Image
+                src={images.concreteScarifyService}
+                alt="man cutting concrete"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1]">
+              <p className="w-full text-center text-black p-2">
+                Lorem ipsum dolor sit.
+              </p>
+            </div>
           </div>
-          <p className="w-full text-center text-black p-2">
-            Lorem ipsum dolor sit.
-          </p>
         </div>
       </div>
     </section>
